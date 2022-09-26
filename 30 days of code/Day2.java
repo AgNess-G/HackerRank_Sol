8ded9d140099121e3640f4790c059888e23cca80
Agnes George <79598409+AgNess-G@users.noreply.github.com>
2022-09-26 21:54:35 +0530

    Create Day2.java

---
+++ 30 days of code/Day2.java
@@ -0,0 +1,17 @@
+class Result {
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function accepts following parameters:
+     *  1. DOUBLE meal_cost
+     *  2. INTEGER tip_percent
+     *  3. INTEGER tax_percent
+     */
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+    // Write your code here
+        System.out.println(Math.round(meal_cost + (tip_percent*meal_cost)/100 + (tax_percent*meal_cost)/100));
+
+    }
+}
