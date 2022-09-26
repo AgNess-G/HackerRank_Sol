838d3c6e8704759a042fe4c23d03f238b8587862
Agnes George <79598409+AgNess-G@users.noreply.github.com>
2022-09-27 00:14:37 +0530

    Create Day24.py

---
+++ 30 days of code/Day24.py
@@ -0,0 +1,29 @@
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        #Write your code here
+        node = head
+        follower=head
+        while(node.next!=None):
+            if(node.data == node.next.data):
+                node.next=node.next.next
+            else:
+                node=node.next
+        return head
